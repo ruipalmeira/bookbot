584ecfcb074cb5b0a6934af33c42fae79d27d85d
Rui Palmeira <ruipalmeira@gmail.com>
2025-10-15 14:58:29 +0100

    feat: removed hardcoded filepath. now uses sys.argv to look for a filepath.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from stats import count_words, count_char, get_book_text, print_sort_dict
+import sys
 def main():
-    file = "books/frankenstein.txt"
+    file = ""
+    if (len(sys.argv) != 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        file = sys.argv[1]
     #get_book_text(file)
     wordcount = count_words(file)
     charcount = print_sort_dict(file)
